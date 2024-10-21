669167a653fde4933b969aca5538dbaf18158770
KevinSanchez0522 <167906890+KevinSanchez0522@users.noreply.github.com>
2024-10-21 17:07:58 -0500

    ultima actualizacion filtro por codigo

--- lugrascolv2/inventario/static/filtrar.js
+++ lugrascolv2/inventario/static/filtrar.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    $('.js-example-basic-single').select2();
     // Función para filtrar la tabla
     document.getElementById('filtro').addEventListener('change', function () {
         var filtro = this.value;
@@ -31,6 +33,26 @@ document.addEventListener("DOMContentLoaded", function() {
                 } else {
                     fila.style.display = 'none';
                 }
+            }
+        });
+    });
+    $('#filtroCodigo').on('change', function() {
+        var filtro = $(this).val(); // Valor seleccionado
+        var filas = $('.tabla tbody tr');
+
+        filas.each(function() {
+            var fila = $(this);
+            var codigoInventario = fila.children('td').first().text(); // Obtener el ID del producto
+
+            // Mostrar u ocultar la fila
+            if (filtro) {
+                if (codigoInventario === filtro) {
+                    fila.show(); // Mostrar fila si coincide con el código
+                } else {
+                    fila.hide(); // Ocultar si no coincide
+                }
+            } else {
+                fila.show(); // Si no hay filtro, mostrar todas las filas
             }
         });
     });
